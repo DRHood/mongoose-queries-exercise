fa146c55fe1604da34f0109ccc040cdedc2f2389
jabyess <james.byess@gmail.com>
2018-09-16 11:16:43 -0400

    seed done, finish exercises

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const db = require('./db/connection')
+const Author = require('./models/Author')
+const Book = require('./models/Book').Book
+const Category = require('./models/Book').Category
 
-console.log(db)
 /* 
-Follow the prompts and write your mongoose queries in here. Make sure you import the models first!
+Follow the prompts and write your mongoose queries in here.
+
+For each query you write, make sure you resolve the promise using .then()
+
+console.log() the result inside the .then() function
+
+for example:
+Author.find().then(author => {
+	console.log(author)
+})
+
+Pay attention to what each query returns. Some will return an array, others return an object.
+
 */
+
+// Get a list of all the authors
+
+
+// Get a list of all the books
+
+
+// Get a list of all the categories
+
+
+// Search for Robert Heinlein by lastName only
+
+
+// Search for a single category with the name of "Fiction"
+
+
+
+
+
+
+
+
+
+
